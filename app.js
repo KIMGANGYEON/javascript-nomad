d06c9b4be6fca726e52b3a088e670190ee6e79ab
KIMGANGYEON <rkddus3177@naver.com>
2024-02-08 22:17:57 +0900

    킁

--- app.js
+++ app.js
@@ -1,13 +1,13 @@
-const age = parseInt(prompt("how old are you?"));
+const h1 = document.querySelector(".hello1");
 
-console.log(isNaN(age));
-
-if (isNaN(age || age < 0)) {
-  console.log("Please wirte a number");
-} else if (age < 18) {
-  console.log("You are too young.");
-} else if (age >= 18 && age <= 50) {
-  console.log("you can eate");
-} else {
-  console.log("you can drink");
+function clickEvent() {
+  h1.classList.toggle("active");
+  //   const clickedClass = "active";
+  //   if (h1.classList.contains(clickedClass)) {
+  //     h1.classList.remove(clickedClass);
+  //   } else {
+  //     h1.classList.add(clickedClass);
+  //   }
+  // }
 }
+h1.addEventListener("click", clickEvent);
